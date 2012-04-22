b9c536609bc3fd0ce9fc9a47d3655476b195d50b
Chris Bevan <schnerbazoink@gmail.com>
2012-04-22 22:47:39 +0100

    Menu tweaks, including sounds

--- menu.py
+++ menu.py
@@ -26,6 +26,8 @@ Many thanks go to:
   sfxr and co)
 - vilya, for convincing me to give this
   competition a go :)
+- My girlfriend Laura, for being
+  understanding throughout the process!
 """.split('\n')
 
 keys_text = """
@@ -47,6 +49,7 @@ class Menu(object):
 		self._next_frame_tick = 0
 		self._big_font = misc.getFont(64)
 		self._mid_font = misc.getFont(32)
+		self._smallish_font = misc.getFont(24)
 		self._options = ['Start Game', 'Credits', 'Quit']
 		self._START_INDEX = 0
 		self._CREDITS_INDEX = 1
@@ -54,6 +57,8 @@ class Menu(object):
 		self._selected_index = self._START_INDEX
 		self._option_count = len(self._options)
 		self._show_credits = False
+		self._move_sound = pygame.mixer.Sound('data/sfx/Menu_Move.ogg')
+		self._select_sound = pygame.mixer.Sound('data/sfx/Menu_Select.ogg')
 		
 	def updateEvents(self):
 		"Returns False iff exited."
@@ -73,11 +78,13 @@ class Menu(object):
 			if event.type == pygame.KEYDOWN:
 				if self._show_credits:
 					if event.key == pygame.K_ESCAPE or event.key == pygame.K_RETURN:
+						self._select_sound.play()
 						self._show_credits = False
 				else:
 					if event.key == pygame.K_ESCAPE:
 						return False
 					if event.key == pygame.K_RETURN:
+						self._select_sound.play()
 						if self._selected_index == self._START_INDEX:
 							self._accepted = True
 						elif self._selected_index == self._CREDITS_INDEX:
@@ -85,10 +92,12 @@ class Menu(object):
 						else:	# _QUIT_INDEX
 							return False
 					elif event.key == pygame.K_KP8 or event.key == pygame.K_UP:
+						self._move_sound.play()
 						if self._selected_index == 0:
 							self._selected_index = self._option_count
 						self._selected_index -= 1
 					elif event.key == pygame.K_KP2 or event.key == pygame.K_DOWN:
+						self._move_sound.play()
 						self._selected_index += 1
 						if self._selected_index >= self._option_count:
 							self._selected_index = 0
@@ -111,11 +120,11 @@ class Menu(object):
 	def renderCredits(self):
 		y = self._screen_rect.centery
 		total_lines = len(credit_text)
-		spacing = 40
+		spacing = 32
 		y -= (total_lines - 2) * spacing / 2
 		for line in credit_text:
 			if line.strip() != "":
-				misc.renderText(line, (50, y), CREDITS_COL, self._mid_font)
+				misc.renderText(line, (150, y), CREDITS_COL, self._smallish_font)
 				y += spacing
 		
 	def renderKeyHelp(self):
